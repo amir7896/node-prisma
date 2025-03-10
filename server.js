e1e232c02246eeacf6f3fa9418cde6de180e2fdf
amir7896 <amirshahzad07896@gmail.com>
2025-03-10 12:46:54 +0500

    Prisma schema updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
 // Start Token Cron Job
-tokenCron.start();
+// tokenCron.start();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
